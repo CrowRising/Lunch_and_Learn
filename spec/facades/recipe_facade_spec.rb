7bb5340b3cd1e700cbe8e72cd4317f95735f08c5
Crow Rising <miscbyrd@gmail.com>
2023-08-06 16:35:42 -0600

    test: add tests for recipe facade

---
+++ spec/facades/recipe_facade_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Recipe Facade" do
+  describe "class methods" do
+    it "returns a list of recipes by country", :vcr do
+      recipes = RecipeFacade.new.get_recipes('Thailand')
+
+      expect(recipes).to be_an Array
+      expect(recipes.first).to be_a Recipe
+      expect(recipes.first.title).to be_a String
+      expect(recipes.first.image).to be_a String
+      expect(recipes.first.url).to be_a String
+      expect(recipes.first.country).to be_a String
+      expect(recipes.first.country).to eq('Thailand')
+      expect(recipes.first.title).to_not be_empty
+      expect(recipes.first.image).to_not be_empty
+      expect(recipes.first.url).to_not be_empty
+      expect(recipes.first.country).to_not be_empty
+      expect(recipes.first.id).to eq('null')
+    end
+  end
+end
