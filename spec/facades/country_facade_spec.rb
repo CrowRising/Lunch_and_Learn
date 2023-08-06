7548400ae3f790fbd4c17dc4e9adfe087900f448
Crow Rising <miscbyrd@gmail.com>
2023-08-06 15:32:51 -0600

    test: add test for country facade

---
+++ spec/facades/country_facade_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe CountryFacade do
+  describe '.get_country' do
+    it 'returns a valid country when is provided', :vcr do
+     country = CountryFacade.get_country('Thailand')
+
+     expect(country).to be_a String
+     expect(country).to eq('Thailand')
+     expect(country).to_not be_empty
+    end
+
+    it 'returns a random country when country is not provided', :vcr do
+     country = CountryFacade.get_country(nil)
+
+     expect(country).to be_a String
+     expect(country).to_not be_empty
+    end
+  end
+end
