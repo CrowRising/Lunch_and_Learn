5b45be7ad1e45084964a563059e1019daf8e14dd
Crow Rising <miscbyrd@gmail.com>
2023-08-06 16:35:14 -0600

    feat: create recipe poro

---
+++ app/poros/recipe.rb
@@ -0,0 +1,11 @@
+class Recipe
+  attr_reader :title, :image, :url, :country, :id
+
+  def initialize(data, country)
+    @title = data[:recipe][:label]
+    @image = data[:recipe][:image]
+    @url = data[:recipe][:url]
+    @country = country
+    @id = 'null'
+  end
+end
