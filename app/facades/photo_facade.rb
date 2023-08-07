548ceb668bfca1f3a65824714301a5983d56a2b9
Crow Rising <miscbyrd@gmail.com>
2023-08-07 14:58:49 -0600

    feat: create photo facade

---
+++ app/facades/photo_facade.rb
@@ -0,0 +1,9 @@
+class PhotoFacade
+
+  def photos_of_country(country)
+    photo_data = PhotoService.new.get_photos(country)
+    photo_data[:results].map do |photo|
+      Photo.new(photo)
+    end
+  end
+end
