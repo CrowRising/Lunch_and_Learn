4ced6c98ad1c7b7af4900683890368c87cd37ec5
Crow Rising <miscbyrd@gmail.com>
2023-08-07 16:25:47 -0600

    wip:make test more robust

--- spec/requests/api/v1/recipes_spec.rb
+++ spec/requests/api/v1/recipes_spec.rb
@@ -9,6 +9,29 @@ RSpec.describe "GET /api/v1/recipes?country=thailand" do
       get "/api/v1/recipes?country=#{country}", headers: headers
 
       expect(response).to be_successful
+
+      recipes = JSON.parse(response.body, symbolize_names: true)
+      expect(recipes).to be_a(Hash)
+      expect(recipes).to have_key(:data)
+      expect(recipes[:data]).to be_a(Array)
+
+      recipes[:data].each do |recipe|
+        expect(recipe).to have_key(:id)
+        expect(recipe[:id]).to eq("null")
+        expect(recipe).to have_key(:type)
+        expect(recipe[:type]).to eq('recipe')
+        expect(recipe).to have_key(:attributes)
+        expect(recipe[:attributes]).to be_a(Hash)
+        expect(recipe[:attributes]).to have_key(:title)
+        expect(recipe[:attributes][:title]).to be_a(String)
+        expect(recipe[:attributes]).to have_key(:image)
+        expect(recipe[:attributes][:image]).to be_a(String) 
+        expect(recipe[:attributes]).to have_key(:url)
+        expect(recipe[:attributes][:url]).to be_a(String) 
+        expect(recipe[:attributes]).to have_key(:country)
+        expect(recipe[:attributes][:country]).to be_a(String)
+        expect(recipe[:attributes].keys.size).to eq(5)
+      end
     end
   end
 end
