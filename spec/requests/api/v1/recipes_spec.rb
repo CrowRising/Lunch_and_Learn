f117b104a77a317ff15d081cb05abdd2c19a5300
Crow Rising <miscbyrd@gmail.com>
2023-08-07 16:31:18 -0600

    test: make recipe test more robust and test for sad and edge cases

--- spec/requests/api/v1/recipes_spec.rb
+++ spec/requests/api/v1/recipes_spec.rb
@@ -33,5 +33,41 @@ RSpec.describe "GET /api/v1/recipes?country=thailand" do
         expect(recipe[:attributes].keys.size).to eq(5)
       end
     end
+
+    it "returns an empty array if no recipes are found", :vcr do
+      country = 'asdfasdfasdf'
+      get "/api/v1/recipes?country=asdfasdfasdf"
+       expect(response).to be_successful
+
+       recipes = JSON.parse(response.body, symbolize_names: true)
+       expect(recipes).to be_a(Hash)
+       expect(recipes).to have_key(:data)
+       expect(recipes[:data]).to be_a(Array)
+       expect(recipes[:data]).to eq([])
+    end
+  end
+
+  it "returns a random country if one is not provided", :vcr do
+    get "/api/v1/recipes"
+
+    expect(response).to be_successful
+    recipes = JSON.parse(response.body, symbolize_names: true)
+
+    recipes[:data].each do |recipe|
+      expect(recipe).to have_key(:id)
+      expect(recipe[:id]).to eq("null")
+      expect(recipe).to have_key(:type)
+      expect(recipe[:type]).to eq('recipe')
+      expect(recipe).to have_key(:attributes)
+      expect(recipe[:attributes]).to be_a(Hash)
+      expect(recipe[:attributes]).to have_key(:title)
+      expect(recipe[:attributes][:title]).to be_a(String) 
+      expect(recipe[:attributes]).to have_key(:image)
+      expect(recipe[:attributes][:image]).to be_a(String) 
+      expect(recipe[:attributes]).to have_key(:url)
+      expect(recipe[:attributes][:url]).to be_a(String)
+      expect(recipe[:attributes]).to have_key(:country)
+      expect(recipe[:attributes][:country]).to be_a(String)
+    end
   end
 end
