57ffa6570de2bd100a809e0bc6163847f447145a
Crow Rising <miscbyrd@gmail.com>
2023-08-08 11:34:47 -0600

    refactor: add to index action

--- app/controllers/api/v1/books_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::BooksController < ApplicationController
-
-  def index
-    books = BookFacade.new(params[:country], params[:quantity]).get_book_data
-    render json: BookSerializer.new(books)
-    
-  end
-end
