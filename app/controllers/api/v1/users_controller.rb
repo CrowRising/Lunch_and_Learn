8a96848201143877c9a53f8464fc233aade482e6
Crow Rising <miscbyrd@gmail.com>
2023-08-08 15:25:48 -0600

    fix: remove one line in controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,8 +2,6 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    user.set_api_key
-    # require 'pry'; binding.pry
     if user.save
       render json: UserSerializer.new(user), status: :created
     else
