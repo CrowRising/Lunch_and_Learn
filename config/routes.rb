e88b48f4364ee86060f6f482f6b741990bda5d1b
Crow Rising <miscbyrd@gmail.com>
2023-08-08 16:17:45 -0600

    chore: add route for faves

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get '/api/v1/learning_resources', to: 'api/v1/learning_resources#index'
   post '/api/v1/users', to: 'api/v1/users#create'
   post '/api/v1/sessions', to: 'api/v1/sessions#create'
+  post '/api/v1/favorites', to: 'api/v1/favorites#create'
 end
