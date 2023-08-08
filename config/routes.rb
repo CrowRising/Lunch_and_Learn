eff9a894da4f9e34662f99952d0da5be99a6563a
Crow Rising <miscbyrd@gmail.com>
2023-08-08 11:35:40 -0600

    refactor: change route name to reflect assignment

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get '/api/v1/recipes', to: 'api/v1/recipes#index'
   get '/api/v1/learning_resources', to: 'api/v1/learning_resources#index'
   post '/api/v1/users', to: 'api/v1/users#create'
+
+  get '/api/v1/book_search', to: 'api/v1/book_search#index'
 end
