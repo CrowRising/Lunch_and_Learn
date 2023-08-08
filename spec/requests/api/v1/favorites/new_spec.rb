5eeacb6fda274af171096095b4a4553408f62e80
Crow Rising <miscbyrd@gmail.com>
2023-08-08 16:27:09 -0600

    test: add sad path

--- spec/requests/api/v1/favorites/new_spec.rb
+++ spec/requests/api/v1/favorites/new_spec.rb
@@ -16,6 +16,35 @@ RSpec.describe "Favorites", type: :request do
 
       expect(response).to be_successful
       expect(response.status).to eq(201)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body).to be_a(Hash)
+      expect(response_body).to have_key(:success)
+      expect(response_body[:success]).to be_a(String)
+      expect(response_body[:success]).to eq("Favorite added successfully")
+    end
+
+    it "returns an error message without an api key" do
+      user_params = {
+        api_key: "7982504938",
+        country: "Thailand",
+        recipe_link: "https://www.allrecipes.com/recipe/246628/thai-red-curry-chicken-soup/",
+        recipe_name: "Thai Red Curry Chicken Soup"
+      }
+      headers = {"CONTENT_TYPE" => "application/json"}
+      post "/api/v1/favorites", headers: headers, params: JSON.generate(user_params)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(401)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body).to be_a(Hash)
+      expect(response_body).to have_key(:error)
+      expect(response_body[:error]).to be_a(String)
+      expect(response_body[:error]).to eq("Unauthorized user")
+      
     end
   end
 end
