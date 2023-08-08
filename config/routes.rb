ad4709ef758963449caec583b3df812a07d01141
Crow Rising <miscbyrd@gmail.com>
2023-08-08 14:24:40 -0600

    chore: add route fro sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/api/v1/recipes', to: 'api/v1/recipes#index'
   get '/api/v1/learning_resources', to: 'api/v1/learning_resources#index'
   post '/api/v1/users', to: 'api/v1/users#create'
+  post '/api/v1/sessions', to: 'api/v1/sessions#create'
 end
