bb1c93e87c8dd741f1e85c42462698223a3deb14
Crow Rising <miscbyrd@gmail.com>
2023-08-08 16:19:05 -0600

    wip:

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:country) }
+    it { should validate_presence_of(:recipe_link) }
+    it { should validate_presence_of(:recipe_title) }
+    it { should belong_to(:user) }
+  end
 end
