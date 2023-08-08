f7f5a757ae3a5465966deefe7e14e7f30f4604fa
Crow Rising <miscbyrd@gmail.com>
2023-08-08 09:31:42 -0600

    refactor: add the rest of the methods

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -5,6 +5,24 @@ class BookFacade
     @quantity = quantity
   end
 
+  def get_book_data
+    Book.new(@location, total_books_found, books)
+  end
+
+  def total_books_found
+    book_data[:numFound]
+  end
+
+  def books
+    book_data[:docs].map do |book_info|
+      {
+        title: book_info[:title],
+        publisher: book_info[:publisher],
+        isbn: book_info[:isbn]
+      }
+    end
+  end
+
   private
 
   def book_service
