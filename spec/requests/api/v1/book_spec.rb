30fdd96e3cff05665999d73e20ca559ed36332b4
Crow Rising <miscbyrd@gmail.com>
2023-08-08 11:36:17 -0600

    refactor: change test to reflect attribute correction for city

--- spec/requests/api/v1/book_spec.rb
+++ spec/requests/api/v1/book_spec.rb
@@ -5,10 +5,35 @@ RSpec.describe 'Book API' do
     it 'returns book data for a given country', :vcr do
      book_facade = BookFacade.new('France', 5)
      allow(BookFacade).to receive(:new).and_return(book_facade) 
-      get '/api/v1/books?country=France&quantity=5'
+      get '/api/v1/book_search?location=France&quantity=5'
 
       expect(response).to be_successful
       expect(response.status).to eq(200)
+
+      book_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(book_data).to be_a(Hash)
+      expect(book_data).to have_key(:data)
+      expect(book_data[:data]).to be_a(Hash)
+      expect(book_data[:data]).to have_key(:id)
+      expect(book_data[:data][:id]).to eq(nil)
+      expect(book_data[:data]).to have_key(:type)
+      expect(book_data[:data][:type]).to eq('books')
+      expect(book_data[:data]).to have_key(:attributes)
+      expect(book_data[:data][:attributes]).to be_a(Hash)
+      expect(book_data[:data][:attributes]).to have_key(:city)
+      expect(book_data[:data][:attributes][:city]).to be_a(String)
+      expect(book_data[:data][:attributes]).to have_key(:total_books_found)
+      expect(book_data[:data][:attributes][:total_books_found]).to be_an(Integer)
+      expect(book_data[:data][:attributes]).to have_key(:books)
+      expect(book_data[:data][:attributes][:books]).to be_an(Array)
+      expect(book_data[:data][:attributes][:books].first).to have_key(:title)
+      expect(book_data[:data][:attributes][:books].first[:title]).to be_a(String)
+      expect(book_data[:data][:attributes][:books].first).to have_key(:publisher)
+      expect(book_data[:data][:attributes][:books].first[:publisher]).to be_a(Array)
+      expect(book_data[:data][:attributes][:books].first).to have_key(:isbn)
+      expect(book_data[:data][:attributes][:books].first[:isbn]).to be_a(Array)
+      expect(book_data[:data][:attributes][:books].first[:isbn].first).to be_a(String)
     end
   end
 end
