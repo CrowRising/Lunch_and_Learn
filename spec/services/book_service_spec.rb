e64f1fed7587dfb8392eafa943eb3680b56d6e79
Crow Rising <miscbyrd@gmail.com>
2023-08-08 09:32:00 -0600

    wip:

--- spec/services/book_service_spec.rb
+++ spec/services/book_service_spec.rb
@@ -9,6 +9,15 @@ RSpec.describe BookService do
       expect(book_response).to be_a(Hash)
       expect(book_response).to have_key(:docs)
       expect(book_response[:docs]).to be_an(Array)
+
+      book = book_response[:docs].first
+  
+      expect(book).to have_key(:title)
+      expect(book[:title]).to be_a(String)
+      expect(book).to have_key(:publisher)
+      expect(book[:publisher]).to be_a(Array)
+      expect(book).to have_key(:isbn)
+      expect(book[:isbn]).to be_a(Array)
     end
   end
 end
