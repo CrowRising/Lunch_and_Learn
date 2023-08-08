a063f083a010b2baca53775da1846f6a01ad701c
Crow Rising <miscbyrd@gmail.com>
2023-08-07 20:28:07 -0600

    wip:

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates_presence_of :password
+  validates :api_key, uniqueness: true
   has_secure_password
+
+  def set_api_key
+    self.api_key ||= SecureRandom.hex(10)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :users
   get '/api/v1/recipes', to: 'api/v1/recipes#index'
   get '/api/v1/learning_resources', to: 'api/v1/learning_resources#index'
+  post '/api/v1/users', to: 'api/v1/users#create'
 end
