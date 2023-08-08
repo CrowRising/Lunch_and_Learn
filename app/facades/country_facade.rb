1e06a8e38bad28b3cab1d6c9d515f3b4815be60d
Crow Rising <miscbyrd@gmail.com>
2023-08-08 11:33:19 -0600

    refactor: add method

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class CountryFacade
+
+  def initialize(country)
+    @country = country
+  end
+
   def self.get_country(country)
     if country.nil? || country.empty?
       country_names = CountryService.new.all_countries.map { |country_data| country_data[:name][:common] }
@@ -8,5 +13,10 @@ class CountryFacade
     else
       country
     end
+  end
+
+  def get_capital(country)
+    country_data = CountryService.new.get_country(country)
+    country_data.first[:capital]
   end
 end
